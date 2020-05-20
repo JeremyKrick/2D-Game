644b437565924010fdc2b03975e47043cbf81c1f
Jeremiah Krick <jeremiah.krick@gmail.com>
2020-05-20 12:27:11 -0700

    add .gitignore

---
+++ Assets/_rotatorscript__.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _rotatorscript__ : MonoBehaviour
+{
+    [Range(-40, 40)]
+    public float rotateSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back * Time.deltaTime * rotateSpeed);
+    }
+}
